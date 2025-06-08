0c7a607136e0f77e5d8d994a23263cfb6ef37a9a
Vipin Yadav <76983380+yvipin6887@users.noreply.github.com>
2025-06-08 22:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
-  // write your code here
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  
+  for (let i = 0; i<l; i++) {
+      result += characters[Math.floor(Math.random()* characters.length)];
+  }
+  
+  return result;
 }
 
 // Do not change the code below.
